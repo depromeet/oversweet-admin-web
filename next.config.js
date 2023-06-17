423ae0c809be0e19c45ab92be32fba3b22b21819
Claire Lee <claire.lee@crashco.io>
2023-06-17 15:52:36 +0900

    CHORE: Specify images hostname in NextJS config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'oversweet.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+      },
+    ],
+  },
+};
