5e4f0bd69a2d2d4b60836b3e82e179951a9bb733
Claire Lee <claire.lee@crashco.io>
2023-06-17 15:56:10 +0900

    FEAT: Implement FranchiseTable

--- src/components/FranchiseTable.tsx
+++ src/components/FranchiseTable.tsx
@@ -1,19 +1,22 @@
 import { Table, TableBody, TableContainer } from '@mui/material'
 import React from 'react'
-import FranchiseTableHeader from './FranchiseTableHeader'
 import useFranchisesData from '@/hooks/queries/useFranchisesData'
 import FranchiseTableRow from './FranchiseTableRow';
+import TableHeader from './TableHeader';
+
+const headerLabel = ['id', '이름', '이미지', '이미지 수정하기', '음료 리스트 보러가기'];
 
 function FranchiseTable() {
   const { data } = useFranchisesData();
+
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <TableContainer sx={{ minWidth: 800, maxWidth: 1200 }}>
         <Table>
-          <FranchiseTableHeader />
+          <TableHeader headers={headerLabel} />
           <TableBody>
             {data?.map((d) =>
-              <FranchiseTableRow key={d.id} {...d} />
+              <FranchiseTableRow key={d.id} data={d} />
             )}
           </TableBody>
         </Table>
--- src/components/FranchiseTableRow.tsx
+++ src/components/FranchiseTableRow.tsx
@@ -1,12 +1,83 @@
 import { IFranchise } from "@/types";
 import { TableCell, TableRow } from "@mui/material"
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useQueryClient } from "react-query";
+import * as queryKeys from '../constant/queryKeys';
+import api from "@/services/api";
+import Image from "next/image";
 
-const FranchiseTableRow = ({ id, name, ImageUrl }: IFranchise) => {
+interface Props {
+  data: IFranchise,
+}
+
+const FranchiseTableRow = ({ data }: Props) => {
+  const router = useRouter();
+  const { id, name } = data;
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (data.ImageUrl) {
+      setImageUrl(data.ImageUrl);
+    }
+  }, [data])
+
+  const moveToDrinkList = () => {
+    router.push(`/franchise/${id}/drinks`);
+  }
+
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [imageUrl, setImageUrl] = useState<string>();
+
+  const handleImageInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedImage(event.target.files[0]);
+    }
+  };
+
+  const handleEditFranchiseImage = async () => {
+    if (selectedImage) {
+      const formData = new FormData();
+      formData.append('image', selectedImage);
+      try {
+        const { data } = await axios.post('/api/uploadImage', formData);
+        const url = data.url;
+        await axios.put(url, selectedImage, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'Access-Control-Allow-Origin': '*',
+          }
+        });
+        await api.patch(`/franchises/${id}`, {
+          imageUrl: url.split('?')[0],
+        });
+        queryClient.invalidateQueries([queryKeys.FRANCHISES]);
+        setImageUrl(url.split('?')[0]);
+      } catch (error) {
+        console.error('Error uploading image:', error);
+      }
+    }
+  };
+
   return (
     <TableRow>
       <TableCell>{id}</TableCell>
       <TableCell>{name}</TableCell>
-      <TableCell>{ImageUrl}</TableCell>
+      <TableCell>
+        {imageUrl
+          ?
+          <Image src={imageUrl} width={100} height={100} alt='' />
+          : '이미지 없음'
+        }
+      </TableCell>
+      <TableCell>
+        <input type="file" accept="image/*" onChange={handleImageInput} />
+        <button onClick={handleEditFranchiseImage}>Upload Image</button>
+      </TableCell>
+      <TableCell>
+        <button onClick={moveToDrinkList}>음료 리스트</button>
+      </TableCell>
     </TableRow>
   )
 }
